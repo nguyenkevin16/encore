2759369055755284fe74a314a642fa9b90a63c7e
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-09 11:28:11 -0800

    Implement user controller and json views

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109190554) do
+ActiveRecord::Schema.define(version: 20170109190932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
