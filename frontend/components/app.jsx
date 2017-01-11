c7d8a2bc8f6a89a34b0df8e011dfff43c554b832
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-11 09:20:09 -0800

    Change splash to welcomepage component '

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import NavbarContainer from './navbar/navbar_container';
-import Splash from './splash/splash';
 
 const App = (props) => (
   <div>
     <NavbarContainer session={props.state.session} />
-    <Splash />
     { props.children }
   </div>
 );
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,11 +4,13 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import AppContainer from './app_container.js';
 import SessionFormContainer from './session/session_form_container';
+import WelcomePage from './welcome/welcome';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path='/' component={AppContainer}>
+        <IndexRoute component={WelcomePage}/>
         <Route path='/login' component={SessionFormContainer}/>
         <Route path='/signup' component={SessionFormContainer}/>
       </Route>
