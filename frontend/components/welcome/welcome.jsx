9ea27b338d12e55c64545088ffe862809fd6b107
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-11 11:55:47 -0800

    Implement basic welcome message

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import WelcomeMessage from './welcome_message';
+
 const WelcomePage = () => (
   <div className='welcome-container'>
     <img src="https://images.pexels.com/photos/96857/pexels-photo-96857.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+
+    <WelcomeMessage />
   </div>
 );
 
---
+++ frontend/components/welcome/welcome_message.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const WelcomeMessage = () => (
+  <div className='welcome-message'>
+    <h2>music for the soul</h2>
+    <h3>when words fail, music speaks...</h3>
+  </div>
+);
+
+export default WelcomeMessage;
