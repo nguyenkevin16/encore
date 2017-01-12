b3b3aa913f913b542dd4b94e07239d1e02f8138e
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-11 18:15:27 -0800

    Fix div in user

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class User extends React.Component {
   render() {
     const { user } = this.props;
-    console.log(user.username);
 
     return (
       <div className='user-show'>
