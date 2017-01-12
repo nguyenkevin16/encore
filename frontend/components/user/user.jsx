3f708602c5dbf70919a327f6f9b2fa2b44185ac9
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-12 15:09:53 -0800

    Add basic cloudinary button to user page

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 import UserImage from './user_image';
 
 class User extends React.Component {
+
+  uploadTrack(e) {
+    e.preventDefault();
+
+    cloudinary.openUploadWidget({
+      cloud_name: window.CLOUDINARY_OPTIONS.cloud_name,
+      upload_preset: window.CLOUDINARY_OPTIONS.upload_preset
+    },
+      (error, result) => {
+
+      }
+    );
+  }
+
   render() {
     const { user } = this.props;
 
@@ -11,6 +25,8 @@ class User extends React.Component {
         <UserImage />
 
         <h1>{ user.username }</h1>
+
+        <button onClick={ this.uploadTrack }>Upload Track</button>
       </div>
     );
   }
