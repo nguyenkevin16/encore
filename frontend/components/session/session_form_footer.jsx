fbaf979e452ae574212354b73094c318fb0fad24
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-13 16:29:36 -0800

    Fix guest login to reroute no matter what  page you are on

--- frontend/components/session/session_form_footer.jsx
+++ frontend/components/session/session_form_footer.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 
-const SessionFormFooter = ({ formFooter, formType, login, closeAndOpenModal, closeModal, router }) => {
+class SessionFormFooter extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const otherFormLink = () => {
-    if (formType === 'login') {
-      return <a onClick={ closeAndOpenModal('signup') }>{'Don\'t have an account?'}</a>;
+    this.otherFormLink = this.otherFormLink.bind(this);
+    this.guestLogin = this.guestLogin.bind(this);
+    this.fillOutAndEnter = this.fillOutAndEnter.bind(this);
+    this.fillField = this.fillField.bind(this);
+  }
+
+  otherFormLink() {
+    if (this.props.formType === 'login') {
+      return <a onClick={ this.props.closeAndOpenModal('signup') }>{'Don\'t have an account?'}</a>;
     } else {
-      return <a onClick={ closeAndOpenModal('login') }>{'Already have an account?'}</a>;
+      return <a onClick={ this.props.closeAndOpenModal('login') }>{'Already have an account?'}</a>;
     }
-  };
+  }
 
-  const guestLogin = () => {
-   const user = { username: 'guest', password: 'password' };
-   login(user);
-   router.push(`users/1`);
-  };
+  guestLogin() {
+    const user = { username: 'guest', password: 'password' };
+    this.props.login(user);
+    this.props.router.push(`users/1`);
+  }
 
-  const fillOutAndEnter = (username, password) => () => {
-    const $usernameInput = $('.session-form-input.username');
-    const $passwordInput = $('.session-form-input.password');
+  fillOutAndEnter(username, password) {
+    return () => {
+      const $usernameInput = $('.session-form-input.username');
+      const $passwordInput = $('.session-form-input.password');
 
-    fillField(username, $usernameInput);
-    setTimeout(
-      () => fillField(password, $passwordInput), (username.length * 80)
-    );
-    setTimeout(
-      () => guestLogin(), ((username.length + password.length) * 90)
-    );
-  };
+      this.fillField(username, $usernameInput);
+      setTimeout(
+        () => this.fillField(password, $passwordInput), (username.length * 80)
+      );
+      setTimeout(
+        () => this.guestLogin(), ((username.length + password.length) * 90)
+      );
+    };
+  }
 
-  const fillField = (value, $field) => {
+  fillField(value, $field) {
     let i = 0;
 
     let timer = setInterval( () => {
@@ -38,15 +48,19 @@ const SessionFormFooter = ({ formFooter, formType, login, closeAndOpenModal, clo
       i++;
       if (i >= value.length) clearInterval(timer);
     }, 70);
-  };
+  }
 
-  return (
-    <div className='session-form-footer'>
-      <a onClick={ closeModal }>Cancel</a>
-      { otherFormLink() }
-      <a onClick={ fillOutAndEnter('guest', 'password') }>Demo account</a>
-    </div>
-  );
-};
+  render() {
+    const { closeModal } = this.props;
+
+    return (
+      <div className='session-form-footer'>
+        <a onClick={ closeModal }>Cancel</a>
+        { this.otherFormLink() }
+        <a onClick={ this.fillOutAndEnter('guest', 'password') }>Demo account</a>
+      </div>
+    );
+  }
+}
 
 export default withRouter(SessionFormFooter);
