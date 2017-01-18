a92b4bf6b29e407bf811a7eaf7e7ff2ac64bdd10
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-18 15:31:47 -0800

    Implement about page

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 const About = () => (
   <div className='about'>
     <div className='about-content'>
-      About goes here.
+      <a href='https://github.com/nguyenkevin16/encore'>
+        <h1> Encore Audio </h1>
+      </a>
+
+      <h3> Encore is a full-stack web application inspired by SoundCloud,
+      built on Ruby on Rails and React.js/Redux. In two weeks,
+      this app will satisfy the following minimum criteria with
+      adequate CSS styling, smooth/bug-free navigation, and seed
+      data: </h3>
+
+      <ul>
+        <li>Hosting on Heroku</li>
+        <li>Sign Up / Sign In / Sign Out / Demo</li>
+        <li>Production README</li>
+        <li>Song CRUD</li>
+        <li>Playing songs with progress bar with continuous play</li>
+        <li>Comments</li>
+        <li> User pages</li>
+      </ul>
     </div>
   </div>
 );
--- frontend/components/welcome/welcome_message.jsx
+++ frontend/components/welcome/welcome_message.jsx
@@ -7,7 +7,6 @@ const WelcomeMessage = () => {
   return (
     <div className='welcome-message'>
       <h2>music for the soul</h2>
-      <h3>when words fail, music speaks...</h3>
       <button onClick={ _redirect }>LISTEN NOW</button>
     </div>
   );
