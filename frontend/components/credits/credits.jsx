09e47528e4b70394ae706ef25b585eee12c44629
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-20 09:45:23 -0800

    Implement basic credits page

--- frontend/components/credits/credits.jsx
+++ frontend/components/credits/credits.jsx
@@ -4,6 +4,30 @@ const Credits = () => (
   <div className='credits'>
     <div className='credits-content'>
       <h1>Acknowledgements</h1>
+
+      <h4>Disclaimer:</h4>
+      <p>
+        This site contains copyrighted material the use of which has
+        not always been specifically authorized by the copyright owner.
+        We are making such material available for personal, educational,
+        non-commercial purposes. We believe this constitutes a 'fair use'
+        of any such copyrighted material as provided for in section 107
+        of the US Copyright Law.
+      </p>
+
+      <h4>Images:</h4>
+      <ul>
+        <li>Splash images were pulled from <a href='www.unsplash.com'>www.unsplash.com</a> and where no credit is required.</li>
+      </ul>
+
+      <h4>Icons:</h4>
+      <ul>
+        <li>Headphone icon made by <a href='http://www.flaticon.com/authors/freepik'>Freepik</a> from <a href='www.flaticon.com'>www.flaticon.com</a></li>
+        <li>Overlay play icon made by <a href='http://www.flaticon.com/authors/madebyoliver'>Madebyoliver</a> from <a href='www.flaticon.com'>www.flaticon.com</a></li>
+        <li>Playbar play icon by <a href='http://www.flaticon.com/authors/madebyoliver'>Madebyoliver</a> from <a href='www.flaticon.com'>www.flaticon.com</a></li>
+        <li>Playbar X icon made by <a href='http://www.flaticon.com/authors/lyolya'>Lyolya</a> from <a href='www.flaticon.com'>www.flaticon.com</a></li>
+        <li>Playbar pause icon made by <a href='http://www.flaticon.com/authors/freepik'>Freepik</a> from <a href='www.flaticon.com'>www.flaticon.com</a></li>
+      </ul>
     </div>
   </div>
 );
