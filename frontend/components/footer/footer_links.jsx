98c10a0684d12e0fa441006fcb30e49115a37af0
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-19 18:59:53 -0800

    Add target blanks to footer links to open in new tab

--- frontend/components/footer/footer_links.jsx
+++ frontend/components/footer/footer_links.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router';
 const FooterLinks = () => (
   <div className='footer-links'>
     <Link to='about'>About</Link>
-    <a href='https://github.com/nguyenkevin16/encore'>Github</a>
-    <a href='https://www.linkedin.com/in/nguyenkevin16/'>LinkedIn</a>
+    <a href='https://github.com/nguyenkevin16/encore' target="_blank">Github</a>
+    <a href='https://www.linkedin.com/in/nguyenkevin16/' target="_blank">LinkedIn</a>
     <Link to='credits'>Credits</Link>
   </div>
 );
