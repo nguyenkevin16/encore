59c78f1e4c199c9591f7ccea5f6aa8a5d20a156d
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-04-06 14:39:42 -0700

    Change welcome message and display to pop more

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -8,6 +8,8 @@ const WelcomePage = () => {
   return (
     <div className='welcome-page'>
       <div className='welcome-container'>
+        <div className='overlay'/>
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_1204/v1491513238/patrick-pilz-146040_gqbcid.jpg'></img>
         <WelcomeMessage/>
       </div>
 
