d33c79f86fb24584b2910988b553498de9a286fd
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-07 21:59:09 -0700

    Add images and edit styling for splash page feature descriptions

--- frontend/components/welcome/welcome_descriptions.jsx
+++ frontend/components/welcome/welcome_descriptions.jsx
@@ -3,18 +3,33 @@ import React from 'react';
 const WelcomeDescriptions = () => (
   <div className='welcome-descriptions'>
     <div className='welcome-subcontainer'>
-      <h3>Create Music</h3>
-      <p>Upload your own music.</p>
+      <div className='welcome-subcontainer-text'>
+        <h3>Create Music</h3>
+        <p>Upload your own music.</p>
+      </div>
+
+      <img src='https://static1.squarespace.com/static/53585febe4b02ff06cdbbba3/t/5537a674e4b08d87cbebaf0d/1429710458367/Music-Studio-Monochrome-Greyscale.jpg?format=1500w'
+        alt='recording studio'/>
     </div>
 
     <div className='welcome-subcontainer'>
-      <h3>Listen to Music</h3>
-      <p>Discover new music or listen to your own.</p>
+      <img src='https://static1.squarespace.com/static/53585febe4b02ff06cdbbba3/t/5537a674e4b08d87cbebaf0d/1429710458367/Music-Studio-Monochrome-Greyscale.jpg?format=1500w'
+        alt='recording studio'/>
+      
+      <div className='welcome-subcontainer-text'>
+        <h3>Listen to Music</h3>
+        <p>Discover new music or listen to your own.</p>
+      </div>
     </div>
 
     <div className='welcome-subcontainer'>
-      <h3>Share Music</h3>
-      <p>Share your music with others.</p>
+      <div className='welcome-subcontainer-text'>
+        <h3>Share Music</h3>
+        <p>Share your music with others.</p>
+      </div>
+
+      <img src='https://static1.squarespace.com/static/53585febe4b02ff06cdbbba3/t/5537a674e4b08d87cbebaf0d/1429710458367/Music-Studio-Monochrome-Greyscale.jpg?format=1500w'
+        alt='recording studio'/>
     </div>
   </div>
 );
