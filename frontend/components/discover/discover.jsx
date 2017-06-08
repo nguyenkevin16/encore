e58c9e4140740a1edebbdb3e9fc5580eb051eb68
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-08 16:21:21 -0700

    Add discover img/message and restyle tracks

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -17,7 +17,18 @@ class Discover extends React.Component {
 
     return (
       <div className='discover'>
-        { tracks }
+        <div className='discover-container'>
+          <img src='http://res.cloudinary.com/nguyenkevin16/image/upload/v1496811138/fineas-anton-136459_sivevq.jpg'></img>
+          <div className='discover-message'>
+            <h1>Discover</h1>
+            <p>Discover new music below.<br/>
+            Try it out!</p>
+          </div>
+        </div>
+        
+        <div className='discover-tracks'>
+          { tracks }
+        </div>
       </div>
     );
   }
