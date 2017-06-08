7b26d3782d7d81f1786ec97ec03181c460c2849d
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-08 16:38:57 -0700

    Fix discover image

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -18,7 +18,7 @@ class Discover extends React.Component {
     return (
       <div className='discover'>
         <div className='discover-container'>
-          <img src='http://res.cloudinary.com/nguyenkevin16/image/upload/v1496811138/fineas-anton-136459_sivevq.jpg'></img>
+          <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1496899161/blown-away_axrkgr.jpg'></img>
           <div className='discover-message'>
             <h1>Discover</h1>
             <p>Discover new music below.<br/>
