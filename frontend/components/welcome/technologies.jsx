ef9681ee35221be9dcd4866a2feceb72ce3b9a17
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-06-07 23:46:58 -0700

    Add featured technologies component to splash page

---
+++ frontend/components/welcome/technologies.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const Technologies = () => (
+  <div className='tech'>
+    <h1>Featured Technologies</h1>
+    
+    <div>
+      <a href='http://rubyonrails.org/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902781/rails_clj9zs.png' alt='Ruby on Rails' />
+        <p>Ruby on Rails</p>
+      </a>
+      
+      <a href='https://facebook.github.io/react/' target="_blank">
+        <img src='http://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902780/react_gi6l5k.svg' alt='ReactJS' />
+        <p>React.js</p>
+      </a>
+
+      <a href='https://nodejs.org/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902780/node_xrbcd6.png' alt='NodeJS' />
+        <p>Node.js</p>
+      </a>
+
+      <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902779/HTML5_vacnsk.png' alt='HTML5' />
+        <p>HTML5</p>
+      </a>
+
+      <a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1496902778/CSS3_ogedms.png' alt='CSS3' />
+        <p>CSS3</p>
+      </a>
+
+    </div>
+    <div>
+
+      <a href='https://webpack.github.io/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902777/webpack_pnbal9.svg' alt='Webpack' />
+        <p>Webpack</p>
+      </a>
+
+      <a href='https://www.postgresql.org/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902778/postgresql_zgkjwj.png' alt='PostgreSQL' />
+        <p>PostgreSQL</p>
+      </a>
+
+      <a href='cloudinary.com/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902778/cloudinary_pih5co.png' alt='Cloudinary' />
+        <p>Cloudinary</p>
+      </a>
+
+      <a href='https://jquery.com/' target="_blank">
+        <img src='http://res.cloudinary.com/nguyenkevin16/image/upload/c_scale,w_300/v1496902778/jQurery_bwnmte.gif' alt='jQuery' />
+        <p>jQuery</p>
+      </a>
+
+      <a href='rspec.info/' target="_blank">
+        <img src='https://res.cloudinary.com/nguyenkevin16/image/upload/v1496902778/RSpec_y71pcu.png' alt='RSpec' />
+        <p>RSpec</p>
+      </a>
+    </div>
+  </div>
+);
+
+export default Technologies;
